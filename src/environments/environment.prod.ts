89de00a2def35b40da9547eeafe8e67a5c846e23
Cristian Ledezma <crisledezma@gmail.com>
2022-10-05 19:07:09 -0600

    Final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlSupaBase: 'https://abxvxsfcdgwzmzpqkeus.supabase.co/rest/v1/',
+  supaBaseApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFieHZ4c2ZjZGd3em16cHFrZXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ2MzIyODcsImV4cCI6MTk4MDIwODI4N30.KWXKJUEvkcrG0Q4_Zd_s-Z_qxQSgplQGPe-GJmQJzsU',
+  supaBaseAuth: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFieHZ4c2ZjZGd3em16cHFrZXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ2MzIyODcsImV4cCI6MTk4MDIwODI4N30.KWXKJUEvkcrG0Q4_Zd_s-Z_qxQSgplQGPe-GJmQJzsU'
 };
